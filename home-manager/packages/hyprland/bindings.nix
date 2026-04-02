64d38290233c96ab6291efc9d78366ff19c0ab96
Passeriform <bhardwajutkarsh.ub@gmail.com>
2026-04-02 15:40:41 +0530

    update brightness directly for ambxst shell

--- home-manager/packages/hyprland/bindings.nix
+++ home-manager/packages/hyprland/bindings.nix
@@ -53,8 +53,8 @@
       ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
       ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
       ", XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
-      ", XF86MonBrightnessUp, exec, brightnessctl -e2 set 5%+"
-      ", XF86MonBrightnessDown, exec, brightnessctl -e2 set 5%-"
+      ", XF86MonBrightnessUp, exec, ambxst brightness +5"
+      ", XF86MonBrightnessDown, exec, ambxst brightness -5"
       ", XF86KbdBrightnessUp, exec, brightnessctl -d *::kbd_backlight set 5%+"
       ", XF86KbdBrightnessDown, exec, brightnessctl -d *::kbd_backlight set 5%-"
     ];    
