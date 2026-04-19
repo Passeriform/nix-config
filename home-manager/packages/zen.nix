0e91ee3bf2a72910e6ba3361a2099575a8dc4532
Passeriform <bhardwajutkarsh.ub@gmail.com>
2026-04-19 08:40:01 +0530

    add nixos custome search engine

--- home-manager/packages/zen.nix
+++ home-manager/packages/zen.nix
@@ -1,5 +1,6 @@
 {
   inputs,
+  pkgs,
   username,
   ...
 }: {
@@ -18,15 +19,22 @@
       "${username}" = {
         isDefault = true;
         settings = {
+          "browser.bookmarks.addedImportButton" = false;
+          "browser.laterrun.enabled" = false;
+          "browser.rights.3.shown" = true;
+          "browser.termsofuse.prefMigrationCheck" = true;
+          "browser.theme.toolbar-theme" = 1;
           "extensions.autoDisableScopes" = 0;
-          "zen.view.compact.enable-at-startup" = true;
-          "zen.view.experimental-no-window-controls" = true;
-          "zen.welcome-screen.seen" = true;
-          "zen.workspaces.continue-where-left-off" = true;
-          "zen.view.show-newtab-button-top" = false;
+          "security.webauth.webauthn_enable_softtoken" = true;
+          "sidebar.visibility" = "hide-sidebar";
           "zen.tabs.show-newtab-vertical" = false;
           "zen.theme.color-prefs.amoled" = true;
           "zen.theme.color-prefs.use-workspace-colors" = true;
+          "zen.view.compact.enable-at-startup" = true;
+          "zen.view.experimental-no-window-controls" = true;
+          "zen.view.show-newtab-button-top" = false;
+          "zen.welcome-screen.seen" = true;
+          "zen.workspaces.continue-where-left-off" = true;
         };
 
         bookmarks = {
@@ -50,6 +58,21 @@
         #     ublock-origin
         #   ];
         # };
+
+        search = {
+          engines = {
+            "NixOS Packages" = {
+              urls = [{ template = "https://search.nixos.org/packages?from=0&size=200&sort=relevance&type=packages&query={searchTerms}"; }];
+              icon = "https://nixos.org/favicon.png";
+              definedAliases = [ "@np" "@nix" ];
+            };
+            "NixOS Options" = {
+              urls = [{ template = "https://search.nixos.org/options?from=0&size=200&sort=relevance&type=packages&query={searchTerms}"; }];
+              icon = "https://nixos.org/favicon.png";
+              definedAliases = [ "@no" ];
+            };
+          };
+        };
       };
     };
   };
